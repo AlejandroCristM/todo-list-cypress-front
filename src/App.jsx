5de89320ec993c371705da1135507df763566f8a
valeria <valeria.granada@evertecinc.com>
2022-09-03 11:07:09 -0500

    test: add cypress

--- src/App.jsx
+++ src/App.jsx
@@ -25,21 +25,23 @@ class App extends Task {
                 style={{ margin: "15px 0" }}
             >
                 <input type="text"
+                       id='input-task'
                        value={this.state.currentTask}
                        required={true}
                        onInput={this.handleChange}
                        placeholder="Añadir una tarea"
                        className="input input-bordered w-full max-w-xs h-6/4"/>
 
-                <button type="submit" className="btn btn-circle btn-ghost text-gray-400">
+                <button
+                    id='form-submit' type="submit" className="btn btn-circle btn-ghost text-gray-400">
                     <svg xmlns="http://www.w3.org/2000/svg" className='h-6 w-6' viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
                 </button>
             </form>
-            <div className="flex flex-col gap-2">
+            <ul className="flex flex-col gap-2">
               {tasks.map((task) => (
-                  <div
+                  <li
                       key={task.id}
-                      className="flex gap-2 p-2 bg-gray-100 rounded-xl w-full justify-between items-center"
+                      className="label-task flex gap-2 p-2 bg-gray-100 rounded-xl w-full justify-between items-center"
                   >
                       <div className="flex gap-2">
                           <input
@@ -64,9 +66,9 @@ class App extends Task {
                     >
                         <svg className='text-red-600' xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
                     </button>
-                  </div>
+                  </li>
               ))}
-            </div>
+            </ul>
           </div>
         </div>
     );
